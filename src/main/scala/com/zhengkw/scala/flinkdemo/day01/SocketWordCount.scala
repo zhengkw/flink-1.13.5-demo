e8c810dc1e894e267bc64b4d814a625f92336d0a
kevin zheng <6987096-zhengkw@users.noreply.gitlab.com>
2022-01-19 16:45:56 +0800

    move file

--- src/main/scala/com/zhengkw/scala/flinkdemo/day01/SocketWordCount.scala
+++ src/main/scala/com/zhengkw/scala/flinkdemo/day01/SocketWordCount.scala
@@ -1,5 +1,8 @@
+package com.zhengkw.scala.flinkdemo.day01
+
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.time.Time
+
 /**
  * @ClassName:SocketWordCount
  * @author: zhengkw
@@ -12,24 +15,16 @@ import org.apache.flink.streaming.api.windowing.time.Time
 object SocketWordCount {
   def main(args: Array[String]): Unit = {
     def main(args: Array[String]) {
-
-      val env = StreamExecutionEnvironment.getExecutionEnvironment
-      val text = env.socketTextStream("localhost", 9999)
-
-      val counts = text.flatMap {
-        _.toLowerCase.split("\\W+") filter {
-          _.nonEmpty
-        }
-      }
-        .map {
-          (_, 1)
-        }
+      // val env = StreamExecutionEnvironment.getExecutionEnvironment
+      val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration())
+      val text = env.socketTextStream("loyx04", 9999)
+      val wc = text.flatMap(_.split(" "))
+        .map((_, 1))
         .keyBy(_._1)
-        .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
         .sum(1)
-
-      counts.print()
-
+      //打印控制台
+      wc.print()
       env.execute("Window Stream WordCount")
     }
   }
+}
